6e8f6801c097cae91de925b5dce2090c65e8e148
PierreGoude <pierre@webbhatt.com>
2017-02-16 13:35:00 +0100

    Update ALLdeauth.sh

--- ALLdeauth.sh
+++ ALLdeauth.sh
@@ -2,7 +2,7 @@
 
 #####################################################################################################################
 #                                                                                                                   #
-#                     This script is written by Pierre aka Linoge, admin of Webbh4tt                                #
+#                     This script is written by Pierre Goude                               #
 # This program is free software; you can redistribute it and/or modify it under the terms of the GNU General Public #
 #                 The author bears no responsibility  for malicious or illegal use.                                 #
 #                                                                                                                   #
